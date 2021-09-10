c7503a6eb1e905528e1c8dcfc8536934c0c696a4
Alexandre Nascimento <alexandre.nascimento@nexodata.com.br>
2021-09-10 19:51:17 -0300

    NXP-308 melhoria pontual

--- ormconfig.js
+++ ormconfig.js
@@ -3,12 +3,12 @@ module.exports = {
   "type": "postgres",
   "port": 5432,
   "url": process.env.DATABASE_URL,
-  "ssl": true,
-  "extra": {
-    "ssl": {
-      "rejectUnauthorized": false
-    }
-  },
+  // "ssl": true,
+  // "extra": {
+  //   "ssl": {
+  //     "rejectUnauthorized": false
+  //   }
+  // },
   "migrations": [
     `./${process.env.MODELS}/database/migrations/*.{ts,js}`
   ],
